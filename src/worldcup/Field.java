e84aedd4156ba0aa36ea01784aa7bf1f808241c9
dave1 <dave1@Mochiles>
2024-09-24 23:15:54 +0200

    add new variable in Field class test2

--- src/worldcup/Field.java
+++ src/worldcup/Field.java
@@ -1,8 +1,8 @@
 package worldcup;
 
 public class Field {
-	int size;
-	int capacity;
+	private int size;
+	private int capacity;
 	
     public void turnLight(){
         System.out.println("Light on");
